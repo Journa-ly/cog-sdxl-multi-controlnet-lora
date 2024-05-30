e7cb4d6b45e2170147c6b77a2d0dd1a279fe8665
Nick Harvey <nick@Nicks-MacBook-Pro.local>
2024-05-30 18:45:48 -0400

    typo

--- weights_downloader.py
+++ weights_downloader.py
@@ -61,11 +61,9 @@ class WeightsDownloader:
 
     @staticmethod
     def download_blobs_in_container(
-        blob_service_client: BlobServiceClient, 
-        container_name: str, 
-        dest_dir: str
+        blob_service_client: BlobServiceClient, container_name: str, dest_dir: str
     ):
-        Ptry:
+        try:
             container_client = blob_service_client.get_container_client(container_name)
             blobs = container_client.list_blobs()
             os.makedirs(dest_dir, exist_ok=True)
