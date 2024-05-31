0ef2a4bac5c4e06d0fb819cd0cab183396fd5bbc
Nick Harvey <nick@Nicks-MacBook-Pro.local>
2024-05-31 13:53:53 -0400

    changing things so it loads only one lora at a time

--- weights_manager.py
+++ weights_manager.py
@@ -30,35 +30,16 @@ class WeightsManager:
         else:
             local_weights_cache = weights
 
-        # load UNET
-        print("Loading fine-tuned model")
-        self.predictor.is_lora = False
-
-        maybe_unet_path = os.path.join(local_weights_cache, "unet.safetensors")
-        if not os.path.exists(maybe_unet_path):
-            print("Does not have Unet. assume we are using LoRA")
-            self.predictor.is_lora = True
-
-        if not self.predictor.is_lora:
-            print("Loading Unet")
-
-            new_unet_params = load_file(
-                os.path.join(local_weights_cache, "unet.safetensors")
-            )
-            # this should return _IncompatibleKeys(missing_keys=[...], unexpected_keys=[])
-            pipe.unet.load_state_dict(new_unet_params, strict=False)
-
-        else:
-            print("Loading Unet LoRA")
+        # If the path ends with .safetensors, treat it as a direct file path
+        if local_weights_cache.endswith(".safetensors"):
+            print("Loading Unet LoRA from specific .safetensors file")
 
             unet = pipe.unet
-
-            tensors = load_file(os.path.join(local_weights_cache, "lora.safetensors"))
+            tensors = load_file(local_weights_cache)
 
             unet_lora_attn_procs = {}
             name_rank_map = {}
             for tk, tv in tensors.items():
-                # up is N, d
                 if tk.endswith("up.weight"):
                     proc_name = ".".join(tk.split(".")[:-3])
                     r = tv.shape[1]
@@ -91,15 +72,82 @@ class WeightsManager:
             unet.set_attn_processor(unet_lora_attn_procs)
             unet.load_state_dict(tensors, strict=False)
 
-        # load text
-        handler = TokenEmbeddingsHandler(
-            [pipe.text_encoder, pipe.text_encoder_2], [pipe.tokenizer, pipe.tokenizer_2]
-        )
-        handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
+            self.predictor.is_lora = True
 
-        # load params
-        with open(os.path.join(local_weights_cache, "special_params.json"), "r") as f:
-            params = json.load(f)
-        self.predictor.token_map = params
+        else:
+            # load UNET
+            print("Loading fine-tuned model")
+            self.predictor.is_lora = False
+
+            maybe_unet_path = os.path.join(local_weights_cache, "unet.safetensors")
+            if not os.path.exists(maybe_unet_path):
+                print("Does not have Unet. assume we are using LoRA")
+                self.predictor.is_lora = True
+
+            if not self.predictor.is_lora:
+                print("Loading Unet")
+
+                new_unet_params = load_file(
+                    os.path.join(local_weights_cache, "unet.safetensors")
+                )
+                pipe.unet.load_state_dict(new_unet_params, strict=False)
+
+            else:
+                print("Loading Unet LoRA")
+
+                unet = pipe.unet
+
+                tensors = load_file(
+                    os.path.join(local_weights_cache, "lora.safetensors")
+                )
+
+                unet_lora_attn_procs = {}
+                name_rank_map = {}
+                for tk, tv in tensors.items():
+                    if tk.endswith("up.weight"):
+                        proc_name = ".".join(tk.split(".")[:-3])
+                        r = tv.shape[1]
+                        name_rank_map[proc_name] = r
+
+                for name, attn_processor in unet.attn_processors.items():
+                    cross_attention_dim = (
+                        None
+                        if name.endswith("attn1.processor")
+                        else unet.config.cross_attention_dim
+                    )
+                    if name.startswith("mid_block"):
+                        hidden_size = unet.config.block_out_channels[-1]
+                    elif name.startswith("up_blocks"):
+                        block_id = int(name[len("up_blocks.")])
+                        hidden_size = list(reversed(unet.config.block_out_channels))[
+                            block_id
+                        ]
+                    elif name.startswith("down_blocks"):
+                        block_id = int(name[len("down_blocks.")])
+                        hidden_size = unet.config.block_out_channels[block_id]
+                    with no_init_or_tensor():
+                        module = LoRAAttnProcessor2_0(
+                            hidden_size=hidden_size,
+                            cross_attention_dim=cross_attention_dim,
+                            rank=name_rank_map[name],
+                        )
+                    unet_lora_attn_procs[name] = module.to("cuda", non_blocking=True)
+
+                unet.set_attn_processor(unet_lora_attn_procs)
+                unet.load_state_dict(tensors, strict=False)
+
+            # load text
+            handler = TokenEmbeddingsHandler(
+                [pipe.text_encoder, pipe.text_encoder_2],
+                [pipe.tokenizer, pipe.tokenizer_2],
+            )
+            handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
+
+            # load params
+            with open(
+                os.path.join(local_weights_cache, "special_params.json"), "r"
+            ) as f:
+                params = json.load(f)
+            self.predictor.token_map = params
 
         self.predictor.tuned_model = True
